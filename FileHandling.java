b4bbb933a4b93bb778a65a118a14c08b297a5558
Aditya Deshmukh <adityadeshmukh628@gmail.com>
2026-07-12 23:18:05 +0530

    Update project

---
+++ FileHandling.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class FileHandling {
+    public static void main(String[] args) throws IOException {
+        File file = new File("student.txt");
+
+        if (file.createNewFile()) {
+            System.out.println("File created");
+        } else {
+            System.out.println("File already exists");
+        }
+
+        FileWriter writer = new FileWriter(file);
+        writer.write("Name: Aditya\n");
+        writer.write("Branch: CSE\n");
+        writer.close();
+
+        FileWriter appendWriter = new FileWriter(file, true);
+        appendWriter.write("CGPA: 7.69\n");
+        appendWriter.close();
+
+        Scanner sc = new Scanner(file);
+
+        while (sc.hasNextLine()) {
+            System.out.println(sc.nextLine());
+        }
+
+        sc.close();
+    }
+}
