44b0b8c9144eb85a5b048c76b1f6f022a0857aa4
Aditya Deshmukh <adityadeshmukh628@gmail.com>
2026-07-18 23:33:14 +0530

    linear search

---
+++ JAVA DSA BASIC PRATICE/Pattern4.java
@@ -0,0 +1,18 @@
+
+
+public class Pattern4 {
+    public static void main(String args[]) 
+    {for (int i=1;i<=6;i++){
+            int count=1;
+               
+            for(int j=1;j<=i-1;j++){
+               System.out.print(count);
+                
+            }
+         
+            System.out.println();
+        }
+    }
+
+    
+}
