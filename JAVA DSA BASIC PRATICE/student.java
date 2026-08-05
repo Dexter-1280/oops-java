c5294429dae93f26b9f077600151d625bf2b0527
Aditya Deshmukh <adityadeshmukh628@gmail.com>
2026-08-05 22:41:27 +0530

    FOR in loop

---
+++ JAVA DSA BASIC PRATICE/student.java
@@ -0,0 +1,27 @@
+class Student {
+    String name;
+    int age;
+
+    Student(String n, int a) {
+        name = n;
+        age = a;
+    }
+
+    void display() {
+        System.out.println(name + " " + age);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Student[] s = {
+            new Student("Aditya", 19),
+            new Student("Rahul", 20),
+            new Student("Aman", 18)
+        };
+
+        for (int i = 0; i < s.length; i++) {
+            s[i].display();
+        }
+    }
+}
